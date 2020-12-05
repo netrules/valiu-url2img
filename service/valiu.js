1ed8cf98238e77fe8a0ef65d69b230a71d3fff3e
Santiago González <santiago.publicmail@gmail.com>
2020-12-05 05:31:48 +0000

    final gitpod

--- service/valiu.js
+++ service/valiu.js
@@ -3,25 +3,28 @@ const url = require('url');
 const fs = require('fs');
 const captureWebsite = require('capture-website');
 
-const thirdPartyURL = 'https://random.dog/woof.json';
+const captureOptions = {
+    launchOptions: {
+        headless: true,
+        args: [ // Disable Chromium's unnecessary SUID sandbox.
+            '--no-sandbox',
+            '--disable-setuid-sandbox'
+        ]
+    }
+};
 
 var valiu = {
     find: async function(req, res, next) {
-        // res.send(url.parse(req.url,true).query.urlLoad);
         let urlLoad = url.parse(req.url,true).query.urlLoad;
 
         // use capture-website to save capture as byte stream
-        await captureWebsite.file('https://sindresorhus.com', 'screenshot.png');
-
-        //let content = await captureWebsite.buffer(urlLoad);
+        let content = await captureWebsite.buffer(urlLoad, captureOptions);
         
         //specify the content type in the response will be an image
         res.writeHead(200,{'Content-type':'image/png'});
 
-        var img = fs.readFileSync('./screenshot.png');
-        res.end(img, 'binary');
-        //res.end(content);
-        //res.send();
+        //send byte load forward to request
+        res.end(content, 'binary');
     }
 };
 
